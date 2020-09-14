2798c6eed026271a395e63cf49071c5ac40248b5
ahmetbicer <ahmtbicertr@gmail.com>
2020-09-14 21:29:17 +0300

    add mutations

--- schema/mutations/mutation.js
+++ schema/mutations/mutation.js
@@ -1,13 +1,43 @@
 const graphql = require("graphql");
+const user = require("../../models/user");
+const AdType = require("../types/adType");
+const UserType = require("../types/userType");
 const {
+  GraphQLString,
   GraphQLObjectType,
+  GraphQLFloat
 } = graphql;
-
 
 const Mutation = new GraphQLObjectType({
     name:"Mutation",
     fields:{
-        
+      addUser: {
+        type: UserType,
+        args: {
+          name: { type: GraphQLString },
+          email: { type: GraphQLString },
+          phone: { type: GraphQLString },
+          password: { type: GraphQLString },
+          rating: { type: GraphQLFloat },
+        },
+        resolve(parent, args) {
+          
+          let _user = new user({
+            name: args.name,
+            email: args.email,
+            phone: args.phone,
+            password: args.password,
+            rating: args.rating
+          })
+          return _user.save()
+        }
+      },
+      addAd: {
+        type: AdType,
+        args:{
+          
+        }
+      } 
     }
 })
 
