a130aa2f3aa8811ae8c9b3ec498c5f26264843fa
ahmetbicer <ahmtbicertr@gmail.com>
2020-09-14 21:29:24 +0300

    add root queries

--- schema/rootQueries/rootQueries.js
+++ schema/rootQueries/rootQueries.js
@@ -1,12 +1,35 @@
 const graphql = require("graphql");
+const ad = require("../../models/ad");
+const animal = require("../../models/animal");
+const user = require("../../models/user");
+const AdType = require("../types/adType");
+const UserType = require("../types/userType");
 const {
   GraphQLObjectType,
+  GraphQLList
 } = graphql;
 
 const RootQuery = new GraphQLObjectType({
     name: "RootQueryType",
     fields:{
-        
+      ads: {
+        type: new GraphQLList(AdType),
+        resolve(parent, args) {
+          return ad.find({})
+        }
+      },
+      users: {
+        type: new GraphQLList(UserType),
+        resolve(parent, args) {
+          return user.find({})
+        }
+      },
+      animals: {
+        type: new GraphQLList(AdType),
+        resolve(parent, args) {
+          return animal.find({})
+        }
+      },
     }
 })
 
