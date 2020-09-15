025a111230edeaca998f9371570e9a3d686f610e
ahmetbicer <ahmtbicertr@gmail.com>
2020-09-15 12:18:16 +0300

    add root queries to get data from id

--- schema/types/animalType.js
+++ schema/types/animalType.js
@@ -7,12 +7,14 @@ const graphql = require("graphql");
 const {
   GraphQLObjectType,
   GraphQLString,
-  GraphQLFloat 
+  GraphQLFloat,
+  GraphQLID
 } = graphql;
 
 const AnimalType = new GraphQLObjectType({
   name: "Animal",
   fields: () => ({
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     age: { type: GraphQLFloat },
     type: { type: AnimalTypeEnum },
